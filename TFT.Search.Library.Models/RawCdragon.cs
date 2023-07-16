eafedec75621228ea1b6a0f39c2f4ca72c08b1d7
Rae Karner <raekarner@gmail.com>
2023-07-16 18:14:19 -0400

    Initialize project with the core models correlating with the Riot "static" JSON data released each patch

---
+++ TFT.Search.Library.Models/RawCdragon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TFT.Search.Library.Models 
+{
+    /// <summary>
+    /// https://raw.communitydragon.org/latest/cdragon/tft/en_us.json
+    /// </summary>
+    public class RawCdragon
+    {
+        public List<object> Items { get; set; }
+        public List<Set> SetData { get; set; }
+    }
+}
---
+++ TFT.Search.Library.Models/Set.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace TFT.Search.Library.Models
+{
+    public class Set
+    {
+        [JsonProperty("number")]
+        [JsonPropertyName("number")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("mutator")]
+        public string Mutator { get; set; }
+
+        [JsonPropertyName("champions")]
+        public List<Champions> Champions { get; set; }
+
+        [JsonPropertyName("traits")]
+        public List<Traits> Traits { get; set; }
+    }
+}
