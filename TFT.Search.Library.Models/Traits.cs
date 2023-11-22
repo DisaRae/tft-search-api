a76ecc466aa1a02b5e42709d781afaec94958874
Rae Karner <raekarner@gmail.com>
2023-11-21 23:26:43 -0500

    Add trait Name and Description

--- TFT.Search.Library.Models/Traits.cs
+++ TFT.Search.Library.Models/Traits.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace TFT.Search.Library.Models
 {
     public class Traits
     {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("desc")]
+        public string Description { get; set; }
     }
 }
